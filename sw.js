05fa1a1b9f1688d8f90800f9cbcfbd6a7cd2e4b3
william0612 <c14200031@john.petra.ac.id>
2023-03-31 09:37:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,41 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/src/css/app.css',
-            '/src/js/app.js'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js',
+          '/src/js/app.js',
+          './about.html',
+          './blog.html',
+          './contact.html',
+          './portfolio-example01.html',
+          './style.css',
+          './js/app.js',
+          './images/about-header.jpg',
+          './images/contact-image.jpg',
+          './images/example-blog01.jpg',
+          './images/example-blog02.jpg',
+          './images/example-blog03.jpg',
+          './images/example-blog04.jpg',
+          './images/example-blog05.jpg',
+          './images/example-blog06.jpg',
+          './images/example-blog07.jpg',
+          './images/example-work01.jpg'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        return res;
+      })
+  );
+});
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(res) {
-          return res;
-        })
-    );
-  });
-    
